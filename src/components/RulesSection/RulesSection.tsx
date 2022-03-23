d97f398da2178c06f642d7dd7e33506b4fb278b0
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-03-23 22:59:17 +0100

    👉 Changed RulesSection positioning.

--- src/components/RulesSection/RulesSection.tsx
+++ src/components/RulesSection/RulesSection.tsx
@@ -5,8 +5,8 @@ import BookIcon from "@mui/icons-material/Book";
 import { ReactSetState } from "@src/types";
 
 const RulesSection: React.FC<{ toggleModal: ReactSetState }> = ({ toggleModal }) => (
-    <div className="relative">
-        <div className="absolute top-0 right-0 flex items-center p-3">
+    <div className="w-full">
+        <div className="float-right flex items-center p-3">
             <div className="cursor-pointer" onClick={() => toggleModal((prevState) => !prevState)}>
                 <BookIcon
                     className="text-green-dark hover:brightness-[.4]"
