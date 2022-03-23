ae905bd65ce00109e0d1816d81dcc417a0483675
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-03-23 15:43:25 +0100

    ✔ Modal window appearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import readFile from "@utils/readFile";
 
 import Titlebar from "./components/Titlebar";
@@ -7,17 +7,25 @@ import Modal from "./components/Modal";
 
 import "./App.scss";
 
-const App: React.FC = () => (
-    <React.Fragment>
-        <Titlebar />
-        <div
-            className="App w-full bg-green-primary overflow-hidden"
-            style={{ height: "calc(100vh - 1.5rem)" }}
-        >
-            <RulesSection />
-            <Modal displayData={readFile("src/assets/data/rules.md")} />
-        </div>
-    </React.Fragment>
-);
+const App: React.FC = () => {
+    const [modalState, setModalState] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Titlebar />
+            <div
+                className="App w-full bg-green-primary overflow-hidden"
+                style={{ height: "calc(100vh - 1.5rem)" }}
+            >
+                <RulesSection toggleModal={setModalState} />
+                <Modal
+                    active={modalState}
+                    toggleModal={setModalState}
+                    displayData={readFile("src/assets/data/rules.md")}
+                />
+            </div>
+        </React.Fragment>
+    );
+};
 
 export default App;
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,13 +3,18 @@ import React from "react";
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
 
-// import classListManip from "@utils/classListManip";
+import { ReactSetState } from "@src/types";
 
 import "./Modal.scss";
 
-const md = require("markdown-it")();
+const md = require("markdown-it")({
+    html: true,
+    typographer: true,
+});
 
 interface ModalData {
+    active?: boolean;
+    toggleModal: ReactSetState;
     displayData?: string;
 }
 
@@ -22,31 +27,29 @@ class Modal extends React.Component<ModalData, { scrollbarClicked: boolean }> {
     }
 
     render() {
+        if (!this.props.active) return null;
+
         return (
-            <div className="fixed z-1300 inset-0 top-6 flex items-center justify-center">
-                <div className="acrylic-bg fixed -z-1 inset-0 top-6"></div>
+            <div
+                className={`${
+                    this.props.active ? "show" : "hide"
+                } fixed z-1300 inset-0 top-6 flex items-center justify-center`}
+            >
+                <div
+                    className="acrylic-bg fixed -z-1 inset-0 top-6"
+                    onClick={() => this.props.toggleModal((prevState) => !prevState)}
+                />
 
                 <div
                     className="content-wrapper absolute w-8/12 bg-white-normal p-4 rounded-xl"
                     style={{ height: "calc(100% * 10 / 12)" }}
                 >
                     <SimpleBar style={{ maxHeight: "100%" }} autoHide={true} timeout={1000}>
-                        <div
-                            className="inserted-content"
+                        <article
+                            className="inserted-content prose prose-slate"
                             dangerouslySetInnerHTML={{ __html: md.render(this.props.displayData) }}
                         />
                     </SimpleBar>
-                    {/* <div
-                        id="edge-scroll-trigger"
-                        onMouseMove={() => {
-                            classListManip.add(["simplebar-vertical"], "simplebar-hover");
-                            classListManip.add(["simplebar-scrollbar"], "simplebar-visible");
-                        }}
-                        onMouseOut={() => {
-                            classListManip.remove(["simplebar-vertical"], "simplebar-hover");
-                            classListManip.remove(["simplebar-scrollbar"], "simplebar-visible");
-                        }}
-                    /> */}
                 </div>
             </div>
         );
