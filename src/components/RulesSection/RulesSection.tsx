01108dc42604a584c0ef422ff8b1a57e9c51dbef
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-03-29 21:04:35 +0200

    👉 Changed RulesSection icon.

--- src/components/RulesSection/RulesSection.tsx
+++ src/components/RulesSection/RulesSection.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-import BookIcon from "@mui/icons-material/Book";
+import BookRoundedIcon from "@mui/icons-material/BookRounded";
 
 import { ReactSetState } from "@src/types";
 
 const RulesSection: React.FC<{ toggleModal: ReactSetState }> = ({ toggleModal }) => (
-    <div className="w-full">
+    <div className="w-full h-[60px]">
         <div className="float-right flex items-center p-3">
             <div className="cursor-pointer" onClick={() => toggleModal((prevState) => !prevState)}>
-                <BookIcon
-                    className="text-green-dark hover:brightness-[.4]"
-                    sx={{ fontSize: 36, filter: "brightness(60%)" }}
+                <BookRoundedIcon
+                    className="text-green-dark hover:brightness-[.5]"
+                    sx={{ fontSize: 36, filter: "brightness(70%)", transition: "all 75ms linear" }}
                 />
             </div>
         </div>
