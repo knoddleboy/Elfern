d621d06b74ebf997daed08b74913f2578308d23e
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-04-09 23:56:04 +0200

    👉 Added some constants

---
+++ src/constants/applicationTheme.ts
@@ -0,0 +1,2 @@
+export const CYRILLIC_THEME_ON: boolean = true;
+export const CYRILLIC_THEME_OFF: boolean = false;
---
+++ src/constants/global.ts
@@ -0,0 +1,11 @@
+/**
+ * Total number of cards in deck (starting from 0).
+ * @value 31
+ */
+export const CARDS_IN_DECK: number = 31;
+
+/**
+ * Defines card's aspect ratio (height / width).
+ * @value 0.719
+ */
+export const CARD_ASPECT_RATIO: number = 0.719;
