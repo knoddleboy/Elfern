74267ecbfba3b8222545a282ffbbda05eadad036
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-04-10 00:01:52 +0200

    Added more types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,28 @@
 import React from "react";
 
 export type ElectronIpcChannel = string;
-export type CSSProps = string;
 export type ReactSetState = React.Dispatch<React.SetStateAction<boolean>>;
+
+/** HEX color model i.e. `#XXX` or `#XXXXXX`. */
+export type ModelHEX =
+    | `#${number}${number}${number}`
+    | `#${number}${number}${number}${number}${number}${number}`;
+
+/** RGB color model i.e. `rgb(A, B, C)`, where _A,B,C_ – numbers in range _[0, 255]_. */
+export type ModelRGB = `rgb(${number}, ${number}, ${number})`;
+
+/** RGBA color model i.e. `rgba(A, B, C, D)`, where
+ * - _A, B, C_ – numbers in range _[0, 255]_;
+ * - _D_ – _[0, 1]_.
+ */
+export type ModelRGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+
+/** Defines type for any number represented in amount of pixels. */
+export type NumberInPX = `${number}px`;
 
 export interface ICustomButtonProps extends React.ComponentPropsWithoutRef<"button"> {
     children: React.ReactNode;
-    styles?: CSSProps;
-    props?: {};
+    className?: string | undefined;
+    otherProps?: React.PropsWithoutRef<HTMLButtonElement>;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
