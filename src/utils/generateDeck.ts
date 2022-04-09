45049608286169e366c6beabd38714eff6989fcc
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-04-10 00:09:59 +0200

    Deck generator utility

---
+++ src/utils/generateDeck.ts
@@ -0,0 +1,21 @@
+import _ from "@utils/_";
+
+export default class Deck {
+    private static ranks = ["ace", "king", "queen", "jack", 10, 9, 8, 7] as const;
+    private static suits = ["clubs", "diamonds", "hearts", "spades"] as const;
+
+    public static createDeck = () => {
+        let deck = [];
+
+        for (let rankIndex = 0; rankIndex < this.ranks.length; rankIndex++) {
+            for (let suitIndex = 0; suitIndex < this.suits.length; suitIndex++) {
+                deck.push({
+                    rank: this.ranks[rankIndex],
+                    suit: this.suits[suitIndex],
+                });
+            }
+        }
+
+        return _.shuffle(deck);
+    };
+}
