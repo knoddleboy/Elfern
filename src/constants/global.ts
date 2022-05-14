a605bb1e3f88d92b3e44207c53d641d3fcde38b3
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-14 23:58:48 +0200

    Global constants

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,3 +1,7 @@
+import media_click from "@assets/media/plastic-click.ogg";
+import media_flip from "@assets/media/flip.ogg";
+import media_shuffle from "@assets/media/shuffle.ogg";
+
 /**
  * Total number of cards in deck (starting from 0).
  * @value 31
@@ -9,3 +13,11 @@ export const CARDS_IN_DECK: number = 31;
  * @value 0.719
  */
 export const CARD_ASPECT_RATIO: number = 0.719;
+
+export const APPLICATION_MEDIA = {
+    click: media_click,
+    flip: media_flip,
+    shuffle: media_shuffle,
+};
+
+export const availableTranslations = ["us", "ua", "de"] as const;
