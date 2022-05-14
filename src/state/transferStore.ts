100a3a04e93db73fa1049ba777787d50ec7b64b9
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-15 00:54:10 +0200

    Transfers store from redux to electron

---
+++ src/state/transferStore.ts
@@ -0,0 +1,24 @@
+import { ipcRenderer } from "electron";
+import { globalSettings } from "../configs";
+
+type StoreType = typeof globalSettings.getStore;
+
+class TransferStore {
+    /**
+     * Get an item from the global config file.
+     * @param key - The key of the item to get. Can be represented in a form of either a key in the store or a dot notation.
+     */
+    static get<Key extends keyof StoreType>(key: Key): StoreType[Key] {
+        return globalSettings.get(key);
+    }
+
+    /**
+     * Send a `value` to the main process to store it locally. Once app is closed, the `value` stores globally in the config file.
+     * @param value - Value to send.
+     */
+    static send(value: Record<string, unknown>): void {
+        ipcRenderer.send("dispatch-main-store", value);
+    }
+}
+
+export default TransferStore;
