2cbce0820e5623d2a68101e385838c4c35762dd9
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-15 00:06:48 +0200

    Now can render modal from inside the component + dynamic lang change

--- src/components/Sidebar/RulesButton/RulesButton.tsx
+++ src/components/Sidebar/RulesButton/RulesButton.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { useSelector } from "react-redux";
+import { State } from "@state/index";
+
+import Modal from "@components/Modal";
 import CustomButton from "@components/CustomButton";
-import BookRoundedIcon from "@mui/icons-material/BookRounded";
+import { ReactComponent as BookRoundedIcon } from "@assets/images/sprites/BookRoundedIcon.svg";
 
-import { ReactSetState } from "@src/types";
+import readFile from "@utils/readFile";
 
-const RulesButton: React.FC<{ toggleModal: ReactSetState }> = ({ toggleModal }) => (
-    <CustomButton className="mb-auto" onClick={() => toggleModal((prevState) => !prevState)}>
-        <BookRoundedIcon
-            className="text-green-dark hover:brightness-[.5]"
-            sx={{ fontSize: 36, filter: "brightness(70%)", transition: "all 75ms linear" }}
-        />
-    </CustomButton>
-);
+import "./RulesButton.scss";
+
+const RulesButton: React.FC<{ tabIndex?: number }> = ({ tabIndex }) => {
+    const [modalState, setModalState] = useState(false);
+
+    const lng = useSelector((state: State) => state.lang);
+
+    return (
+        <React.Fragment>
+            {modalState && (
+                <Modal
+                    isOpen={modalState}
+                    toggleModal={setModalState}
+                    displayData={readFile(`src/assets/locales/${lng}/rules.md`)}
+                />
+            )}
+            <CustomButton
+                className="mb-auto"
+                onClick={() => setModalState((prevState) => !prevState)}
+                tabIndex={tabIndex}
+            >
+                <BookRoundedIcon className="RulesButton w-[1em] h-[1em] fill-green-800 brightness-75 hover:brightness-[60%] text-4xl" />
+            </CustomButton>
+        </React.Fragment>
+    );
+};
 
 export default RulesButton;
