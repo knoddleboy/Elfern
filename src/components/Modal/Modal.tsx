bcaa292d4e1d833b276b5da47aa9e5a6eb605ffc
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-15 00:17:20 +0200

    👉 All functionality was moved to DialogWrapper component
    Added ability to choose modal size, based on the content

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import SimpleBar from "simplebar-react";
 import "simplebar/dist/simplebar.min.css";
 
-import CustomButton from "@components/CustomButton";
-import { ReactSetState } from "@src/types";
+import DialogWrapper from "@components/DialogWrapper";
+import ModalCloseButton from "./ModalCloseButton";
 
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { ReactSetState } from "@src/types";
 
 import "./Modal.scss";
 
@@ -17,90 +16,41 @@ const md = require("markdown-it")({
 });
 
 interface IModalProps {
+    size?: `${number}/${number}`;
     isOpen: boolean;
     toggleModal: ReactSetState;
-    displayData: string;
+    displayData: string | JSX.Element;
 }
 
-interface IModalState {
-    fadeType: string | null;
-}
-
-const modalRoot = document.getElementById("modal-root") as Element;
-
-class Modal extends React.Component<IModalProps, IModalState> {
-    constructor(props: IModalProps) {
-        super(props);
-        this.state = {
-            fadeType: null,
-        };
-    }
-
-    componentDidMount(): void {
-        window.addEventListener("keydown", this.escapeListener, false);
-        setTimeout(() => this.setState({ fadeType: "in" }), 0);
-    }
-
-    componentDidUpdate(prevProps: IModalProps): void {
-        if (!this.props.isOpen && prevProps.isOpen) {
-            this.setState({ fadeType: "out" });
-        }
-    }
-
-    componentWillUnmount(): void {
-        window.removeEventListener("keydown", this.escapeListener, false);
-    }
-
-    setFadeOut = (): void => {
-        this.setState({ fadeType: "out" });
-        setTimeout(() => {
-            this.props.toggleModal((prevState) => !prevState);
-        }, 300);
-    };
-
-    handleCloseClick = (e: React.MouseEvent<HTMLDivElement | HTMLButtonElement>): void => {
-        if (e) e.preventDefault();
-        this.setFadeOut();
-    };
-
-    escapeListener = (
-        e: React.KeyboardEvent<HTMLDivElement | HTMLButtonElement> | KeyboardEvent
-    ): void => {
-        if (e.key === "Escape") {
-            this.setFadeOut();
-        }
-    };
-
-    render() {
-        return ReactDOM.createPortal(
+const Modal: React.FC<IModalProps> = ({ size, isOpen, toggleModal, displayData }) => {
+    return (
+        <DialogWrapper isOpen={isOpen} toggle={toggleModal} centerContent={true}>
             <div
-                className={`fade-${this.state.fadeType} fixed z-[1200] inset-0 top-6 flex items-center justify-center`}
+                className="modal-content absolute bg-white-normal p-4 rounded-xl"
+                style={{
+                    width: `calc(100% * ${size || "8 / 12"})`,
+                    height: size ? "fit-content" : "calc(100% * 10 / 12)",
+                    // aspectRatio: size ? "29 / 25" : "auto",
+                }}
             >
-                <div
-                    className="acrylic-bg fixed -z-1 inset-0 top-6"
-                    onClick={this.handleCloseClick}
-                />
-
-                <div
-                    className="modal-content absolute w-8/12 bg-white-normal p-4 rounded-xl"
-                    style={{ height: "calc(100% * 10 / 12)" }}
-                >
-                    <SimpleBar style={{ maxHeight: "100%" }} autoHide={true} timeout={1000}>
-                        <article
-                            className="inserted-content prose prose-slate max-w-full"
-                            dangerouslySetInnerHTML={{ __html: md.render(this.props.displayData) }}
-                        />
-                        <div className="w-full flex justify-center">
-                            <CustomButton className="modal-close" onClick={this.handleCloseClick}>
-                                <CloseRoundedIcon sx={{ fontSize: 32 }} />
-                            </CustomButton>
-                        </div>
-                    </SimpleBar>
-                </div>
-            </div>,
-            modalRoot
-        );
-    }
-}
+                <SimpleBar style={{ height: "100%" }} autoHide={true} timeout={1000}>
+                    {typeof displayData === "string" ? (
+                        <React.Fragment>
+                            <article
+                                className="inserted-content prose prose-slate max-w-full"
+                                dangerouslySetInnerHTML={{
+                                    __html: md.render(displayData),
+                                }}
+                            />
+                            <ModalCloseButton className="w-full flex justify-center" />
+                        </React.Fragment>
+                    ) : (
+                        displayData
+                    )}
+                </SimpleBar>
+            </div>
+        </DialogWrapper>
+    );
+};
 
 export default Modal;
