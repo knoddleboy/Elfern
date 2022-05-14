e5388d11cd36ba28069c090b8d00d997c5ade4fd
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-14 23:55:48 +0200

    useTranslation hook
    Lets to use translation from JSON using dot-notation.

---
+++ src/utils/hooks/useTranslation.ts
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { State } from "@state/index";
+
+import { bindKeyValuePairs, getByDotNotation } from "@utils/utils";
+import { availableTranslations } from "@constants/global";
+
+import us from "@assets/locales/us/translation.json";
+import ua from "@assets/locales/ua/translation.json";
+import de from "@assets/locales/de/translation.json";
+
+const langs = bindKeyValuePairs(availableTranslations, [us, ua, de]);
+
+const useTranslation = () => {
+    const currentLang = useSelector((state: State) => state.lang);
+
+    const t = (key: string) => {
+        return getByDotNotation(langs[currentLang] as Record<string, any>, key, true);
+    };
+
+    return {
+        t: t,
+        lang: currentLang,
+    };
+};
+
+export default useTranslation;
