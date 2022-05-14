40ccb187878340ff5830266e35ae21394add6c86
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-14 23:52:59 +0200

    Create useWindowSize.ts

---
+++ src/utils/hooks/useWindowSize.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+import useEventListener from "./useEventListener";
+
+interface WindowSize {
+    width: number;
+    height: number;
+}
+
+const useWindowSize = (): WindowSize => {
+    const [windowSize, setWindowSize] = useState<WindowSize>({
+        width: 0,
+        height: 0,
+    });
+
+    const handleSize = () => {
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+    };
+
+    useEventListener("resize", handleSize);
+
+    // Set size at the first load
+    useEffect(() => {
+        handleSize();
+    }, []);
+
+    return windowSize;
+};
+
+export default useWindowSize;
