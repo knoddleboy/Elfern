94eabb0bdca6f1e0a1cc0df89d49747b978780a0
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-15 00:20:11 +0200

    Now application runs on redux state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
 import React, { useState } from "react";
-import readFile from "@utils/readFile";
+import { Provider } from "react-redux";
+
+import { store } from "./state";
+import { ipcRenderer } from "electron";
 
 import Titlebar from "@components/Titlebar";
-import Modal from "@components/Modal";
 import Sidebar from "@components/Sidebar";
 import MainMenu from "@src/components/MainMenu";
 import GameInterface from "@components/GameInterface";
 
 import "./App.scss";
 
+// Send configured store to the main process
+ipcRenderer.send("dispatch-main-store", store.getState());
+
 const App: React.FC = () => {
-    const [modalState, setModalState] = useState(false);
+    // Defines the state of the main (start) menu
     const [mainMenuIsRendered, setMainMenuRenderState] = useState(true);
 
     return (
         <React.Fragment>
             <Titlebar />
-            <div className="App w-full h-[calc(100vh-1.5rem)] bg-green-primary overflow-hidden">
-                {modalState && (
-                    <Modal
-                        isOpen={modalState}
-                        toggleModal={setModalState}
-                        displayData={readFile("src/assets/data/rules.md")}
-                    />
-                )}
-                {mainMenuIsRendered ? (
-                    <MainMenu render={setMainMenuRenderState} />
-                ) : (
-                    <GameInterface />
-                )}
-                <Sidebar toggleModal={setModalState} />
-            </div>
+            <Provider store={store}>
+                <div className="App w-full h-[calc(100vh-1.5rem)] bg-green-500 overflow-hidden">
+                    {mainMenuIsRendered ? <MainMenu render={setMainMenuRenderState} /> : <GameInterface />}
+                    <Sidebar mainMenuState={mainMenuIsRendered} />
+                </div>
+            </Provider>
         </React.Fragment>
     );
 };
