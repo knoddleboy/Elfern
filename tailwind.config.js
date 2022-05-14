8527f2036846786fbfc981617fb416329063a6ac
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-14 23:48:02 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,33 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         colors: {
-            transparent: "rgba(0, 0, 0, 0.5)",
             white: {
-                normal: "#ffffff",
-                grey: "#e1e2e1",
-                text: "#e2f1ff",
-            },
-            dark: {
-                800: "#313131",
-                600: "#4a4c50",
+                normal: "var(--white-normal)",
+                text: "var(--white-text)",
+                grey: "var(--white-greyish)",
             },
             green: {
-                dark: "#006717",
-                primary: "#329e47",
-                400: "#5bb16c",
-                light: "#2b7b3b",
-                200: "rgba(43, 123, 59, 0.3)",
+                800: "var(--green-800)",
+                500: "var(--green-500)",
+                400: "var(--green-400)",
+                300: "var(--green-300)",
+                200: {
+                    alpha: "var(--green-200-alpha)",
+                },
             },
             blue: {
-                400: "#1a88c4",
-                500: "#0077B9",
-                600: "#0072b3",
-                700: "#005382",
+                700: "var(--blue-700)",
+                600: "var(--blue-600)",
+                500: "var(--blue-500)",
+                400: "var(--blue-400)",
+                300: "var(--blue-300)",
+            },
+            dark: {
+                800: "var(--dark-800)",
+                600: "var(--dark-600)",
+            },
+            alpha: {
+                5: "var(--alpha-5)",
             },
         },
         extend: {},
