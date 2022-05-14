29cc9f0f541bf5679ba901df0f02605cb3dd93ad
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-05-15 00:01:37 +0200

    Now sidebar changes its direction based on game state

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,18 +3,24 @@ import React from "react";
 import RulesButton from "./RulesButton";
 import SettingsButton from "./SettingsButton";
 
-import { ReactSetState } from "@src/types";
+interface ISidebar {
+    mainMenuState: boolean;
+}
 
-const Sidebar: React.FC<{ toggleModal: ReactSetState }> = ({ toggleModal }) => (
-    <div
-        className="
-        w-auto h-[calc(100%-24px)]
-        fixed right-0 bottom-0 p-3
-        flex flex-col"
-    >
-        <RulesButton toggleModal={toggleModal} />
-        {/* <SettingsButton /> */}
-    </div>
+const Sidebar: React.FC<ISidebar> = ({ mainMenuState }) => (
+    <React.Fragment>
+        {mainMenuState ? (
+            <div className="w-auto h-[calc(100%-24px)] fixed right-0 bottom-0 p-3 flex flex-col">
+                <RulesButton tabIndex={4} />
+                <SettingsButton tabIndex={5} />
+            </div>
+        ) : (
+            <div className="w-full fixed left-0 top-0 p-3 mt-6 flex flex-row-reverse justify-between">
+                <RulesButton tabIndex={5} />
+                <SettingsButton tabIndex={4} />
+            </div>
+        )}
+    </React.Fragment>
 );
 
 export default Sidebar;
