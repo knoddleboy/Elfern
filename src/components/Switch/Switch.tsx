61b9a85ba3865be2146b08cc7805c97b18c5ef33
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:28:39 +0200

    Add tab focus for switch thumb

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import AudioClick from "@components/AudioClick";
 import { APPLICATION_MEDIA } from "@constants/global";
@@ -14,6 +14,34 @@ interface ISwitch {
 const Switch: React.FC<ISwitch> = ({ size, onChange, defaultChecked }) => {
     const [switchState, setSwitchState] = useState(defaultChecked || false);
     const inputRef = useRef<HTMLInputElement>(null);
+    const trackRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function addThumbFocus(e: KeyboardEvent) {
+            if (!(e.key === "Tab")) return;
+            if (trackRef.current && document.activeElement === inputRef.current) {
+                trackRef.current.classList.add("switch-focused");
+            }
+        }
+
+        function removeThumbFocus(e: Event | KeyboardEvent) {
+            // Omit Enter and Space since they don't
+            if (e instanceof KeyboardEvent && (e.key === "Enter" || e.key === " ")) return;
+            if (trackRef.current && document.activeElement === inputRef.current) {
+                trackRef.current.classList.remove("switch-focused");
+            }
+        }
+
+        document.addEventListener("keyup", addThumbFocus);
+        document.addEventListener("keydown", removeThumbFocus);
+        document.addEventListener("mousedown", removeThumbFocus);
+
+        return () => {
+            document.removeEventListener("keyup", addThumbFocus);
+            document.removeEventListener("keydown", removeThumbFocus);
+            document.removeEventListener("mousedown", removeThumbFocus);
+        };
+    });
 
     const handleChange = () => {
         setSwitchState((prevState) => !prevState);
@@ -21,11 +49,9 @@ const Switch: React.FC<ISwitch> = ({ size, onChange, defaultChecked }) => {
     };
 
     return (
-        <AudioClick sound={APPLICATION_MEDIA.click}>
+        <AudioClick sound={switchState ? APPLICATION_MEDIA.switchOff : APPLICATION_MEDIA.switchOn} alwaysOn={true}>
             <div
-                className={`switch-root ${
-                    switchState ? "switch-checked" : ""
-                } relative z-0 cursor-pointer overflow-hidden`}
+                className={`switch-root ${switchState ? "switch-checked" : ""} relative z-0 cursor-pointer`}
                 style={{
                     width: size ? 2 * size + "rem" : "2.5rem",
                     height: size ? size + "rem" : "1.25rem",
@@ -53,7 +79,7 @@ const Switch: React.FC<ISwitch> = ({ size, onChange, defaultChecked }) => {
                         }}
                     ></div>
                 </div>
-                <div className={`switch-track w-full h-full rounded-full cursor-pointer -z-[1]`}></div>
+                <div className={`switch-track w-full h-full rounded-full cursor-pointer -z-[1]`} ref={trackRef}></div>
             </div>
         </AudioClick>
     );
