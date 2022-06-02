adf32b9667d0b8b8a729fec5a1c0cc48ad8c5aa8
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:26:55 +0200

    Separate defaults

--- src/configs.js
+++ src/configs.js
@@ -1,13 +1,32 @@
 const Store = require("./utils/Store/index");
 
+const storeDefaults = {
+    ENABLE_AUDIO: true,
+    LANGUAGE: "us",
+    ROUND_STATS: {
+        current: 1,
+        max: 1,
+    },
+    INITIAL_SETUP: true,
+    TIMER_STATE: {
+        time: 0,
+        start: false,
+        pause: false,
+        resume: false,
+        reset: false,
+    },
+    CURRENT_SCORE: {
+        player: 0,
+        opponent: 0,
+    },
+};
+
 const globalSettings = new Store({
     configName: "settings",
-    defaults: {
-        ENABLE_AUDIO: true,
-        LANGUAGE: "us",
-    },
+    defaults: storeDefaults,
 });
 
 module.exports = {
     globalSettings,
+    storeDefaults,
 };
