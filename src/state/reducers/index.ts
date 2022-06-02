bb411d29e645fdb54edf8655c53169d1ed4a1f1b
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:24:22 +0200

    Update to constant names

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,10 +1,18 @@
 import { combineReducers } from "redux";
 import audioReducer from "./audio.reducer";
 import langReducer from "./lang.reducer";
+import roundStatsReducer from "./roundStats.reducer";
+import initialSetupReducer from "./initialSetup.reducer";
+import timerStateReducer from "./timerState.reducer";
+import currentScoreReducer from "./currentScore.reducer";
 
 const reducers = combineReducers({
-    audio: audioReducer,
-    lang: langReducer,
+    ENABLE_AUDIO: audioReducer,
+    LANGUAGE: langReducer,
+    ROUND_STATS: roundStatsReducer,
+    INITIAL_SETUP: initialSetupReducer,
+    TIMER_STATE: timerStateReducer,
+    CURRENT_SCORE: currentScoreReducer,
 });
 
 export type State = ReturnType<typeof reducers>;
