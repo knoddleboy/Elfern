a4445ffe2ea2a94ff3ad7ecbc090c0a2839120e1
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:26:14 +0200

    Update global.ts

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,6 +1,11 @@
-import media_click from "@assets/media/plastic-click.ogg";
+import media_click from "@assets/media/switch-on.ogg";
 import media_flip from "@assets/media/flip.ogg";
 import media_shuffle from "@assets/media/shuffle.ogg";
+import switch_on from "@assets/media/switch-on.ogg";
+import switch_off from "@assets/media/switch-off.ogg";
+import winscreen from "@assets/media/winscreen.ogg";
+import losescreen from "@assets/media/losescreen.ogg";
+import count_up from "@assets/media/count-up.ogg";
 
 /**
  * Total number of cards in deck (starting from 0).
@@ -18,6 +23,21 @@ export const APPLICATION_MEDIA = {
     click: media_click,
     flip: media_flip,
     shuffle: media_shuffle,
+    switchOn: switch_on,
+    switchOff: switch_off,
+    winscreen: winscreen,
+    losescreen: losescreen,
 };
 
 export const availableTranslations = ["us", "ua", "de"] as const;
+
+export const EMOJIS = {
+    win: ["🎉", "🥂", "🍕"],
+    lose: ["😒", "🤷‍♀️", "😢"],
+};
+
+export const DETERMINING_BATCH_WINNER_TIME = 4200;
+
+export const CARDS_WITHOUT_SQUEEZING = 16;
+
+export const NO_CARD_AVAILABLE = -2;
