75b279eca0b15292bf39f0c10420418043d2530f
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:35:25 +0200

    Update AudioClick.tsx

--- src/components/AudioClick/AudioClick.tsx
+++ src/components/AudioClick/AudioClick.tsx
@@ -5,11 +5,12 @@ import useAudio from "@utils/hooks/useAudio";
 interface IClickWrapper {
     sound: string;
     preventAudio?: boolean;
+    alwaysOn?: boolean;
     children: React.ReactElement<any>;
 }
 
-const AudioClick: React.FC<IClickWrapper> = ({ sound, preventAudio, children }) => {
-    const [toggle] = useAudio(sound);
+const AudioClick: React.FC<IClickWrapper> = ({ sound, preventAudio, children, alwaysOn = false }) => {
+    const [toggle] = useAudio(sound, alwaysOn);
 
     const clickWithSound = () => {
         const prevClickHandler = children.props.onClick;
