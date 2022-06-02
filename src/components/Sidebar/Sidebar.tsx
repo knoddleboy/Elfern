222512d0ab7d5ff2569bd4982a7b7ba99ed94b50
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:29:24 +0200

    Rearrange focus in buttons

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,13 +11,13 @@ const Sidebar: React.FC<ISidebar> = ({ mainMenuState }) => (
     <React.Fragment>
         {mainMenuState ? (
             <div className="w-auto h-[calc(100%-24px)] fixed right-0 bottom-0 p-3 flex flex-col">
-                <RulesButton tabIndex={4} />
-                <SettingsButton tabIndex={5} />
+                <RulesButton tabIndex={1} />
+                <SettingsButton tabIndex={2} />
             </div>
         ) : (
             <div className="w-full fixed left-0 top-0 p-3 mt-6 flex flex-row-reverse justify-between">
-                <RulesButton tabIndex={5} />
-                <SettingsButton tabIndex={4} />
+                <RulesButton tabIndex={2} />
+                <SettingsButton tabIndex={1} />
             </div>
         )}
     </React.Fragment>
