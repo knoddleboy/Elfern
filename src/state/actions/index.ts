b3a65bf2d99e21199145ae911f3dded6f1948ef1
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:24:44 +0200

    Update index.ts

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -10,4 +10,41 @@ export interface ISetTranslation {
     payload: AvailableCountryCodes;
 }
 
-export type Action = IToggleAudio | ISetTranslation;
+export interface ISetRoundStats {
+    type: ActionType.ROUND_STATS;
+    payload: {
+        current: number;
+        max: number;
+    };
+}
+
+interface IToggleInitialSetup {
+    type: ActionType.IS_INITIAL_SETUP;
+}
+
+export interface ISetTimerState {
+    type: ActionType.TIMER_STATE;
+    payload: {
+        time: number;
+        start: boolean;
+        pause: boolean;
+        resume: boolean;
+        reset: boolean;
+    };
+}
+
+export interface ISetCurrentScore {
+    type: ActionType.CURRENT_SCORE;
+    payload: {
+        player: number;
+        opponent: number;
+    };
+}
+
+export type Action =
+    | IToggleAudio
+    | ISetTranslation
+    | ISetRoundStats
+    | IToggleInitialSetup
+    | ISetTimerState
+    | ISetCurrentScore;
