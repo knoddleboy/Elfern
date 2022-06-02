3170601525e4023b1b32001b6b08665a0cfccef0
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:36:43 +0200

    ✔ Alert component
    Used when notifying that either player or opponent has no cards to answer

---
+++ src/components/GameInterface/Alert/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import useMountTransition from "@utils/hooks/useMountTransition";
+
+import "./Alert.scss";
+
+interface IAlert {
+    isMounted: boolean;
+    isDefaultShown?: boolean;
+    msg: string;
+}
+
+const Alert: React.FC<IAlert> = ({ isMounted, isDefaultShown = false, msg }) => {
+    const transitionedIn = useMountTransition(isMounted || isDefaultShown, 1000);
+
+    return transitionedIn || isMounted ? (
+        <div
+            className={`Alert ${isMounted && "alert-visible"} ${
+                transitionedIn && "alert-in"
+            } absolute w-[inherit] mt-6 p-3 top-0 right-0 text-center z-[1100]`}
+        >
+            <span
+                className="
+            px-5 py-2 rounded-xl
+            bg-white-normal text-lg text-dark-700 font-medium
+            brightness-110"
+            >
+                {msg}
+            </span>
+        </div>
+    ) : null;
+};
+
+export default Alert;
