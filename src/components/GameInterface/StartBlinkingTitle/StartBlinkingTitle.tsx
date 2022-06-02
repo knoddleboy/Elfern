d285a2fc76376d6fbf7be2d522cd06c482783e86
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-02 21:42:50 +0200

    Now if it's first game, ask to enter rounds number

--- src/components/GameInterface/StartBlinkingTitle/StartBlinkingTitle.tsx
+++ src/components/GameInterface/StartBlinkingTitle/StartBlinkingTitle.tsx
@@ -1,6 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "@state/index";
 
 import useTranslation from "@utils/hooks/useTranslation";
+import useWindowSize from "@utils/hooks/useWindowSize";
+
+import RoundsNumberSetterModal from "../RoundsNumberSetterModal";
 
 import { ReactSetState } from "@src/types";
 
@@ -11,11 +18,15 @@ interface IStartBlinkingTitleProps {
     shouldStartGame: ReactSetState;
 }
 
-const StartBlinkingTitle: React.FC<IStartBlinkingTitleProps> = ({
-    elementRef,
-    shouldStartGame,
-}) => {
+const StartBlinkingTitle: React.FC<IStartBlinkingTitleProps> = ({ elementRef, shouldStartGame }) => {
+    const dispatch = useDispatch();
+    const { toggleInitialSetup } = bindActionCreators(actionCreators, dispatch);
+    const isInitialSetup = useSelector((state: State) => state.INITIAL_SETUP);
+
     const { t } = useTranslation();
+    const { width } = useWindowSize();
+
+    const [showRoundSetter, setShowRoundSetter] = useState(false);
 
     useEffect(() => {
         // Create node that points to provided ref element
@@ -27,19 +38,22 @@ const StartBlinkingTitle: React.FC<IStartBlinkingTitleProps> = ({
         // Event handler
         const titleStateHandler = (e: MouseEvent | KeyboardEvent) => {
             const target = e.target as HTMLElement;
-            // Omit alt and tab clicks
 
             // TODO: remove e.key === "r"
             if (
                 (e instanceof MouseEvent && target.closest(".PlayAreaSidebarButtons")) ||
                 (e instanceof KeyboardEvent &&
-                    (e.key === "Escape" ||
-                        e.key === "Alt" ||
-                        e.key === "Tab" ||
-                        e.key === "Control" ||
-                        e.key === "r"))
+                    (e.key === "Escape" || e.key === "Alt" || e.key === "Tab" || e.key === "Control" || e.key === "r")) ||
+                document.getElementById("modal-root")?.firstChild
             )
                 return;
+
+            // If this is the first time after starting an application the user starts game, we ask him/her to
+            // enter the number of rounds and we set isInitialSetup to false to indicate that first setup have
+            // already been fired and we do not need to ask the user to enter the number of rounds again later
+            // (since whenever he/she decides to start a new game, round setting window is showed)
+            if (isInitialSetup) return setShowRoundSetter(true);
+
             shouldStartGame((prevState) => !prevState);
         };
 
@@ -52,10 +66,25 @@ const StartBlinkingTitle: React.FC<IStartBlinkingTitleProps> = ({
             window.removeEventListener("keydown", titleStateHandler);
             node.removeEventListener("click", titleStateHandler);
         };
-    }, [elementRef, shouldStartGame]);
+    }, []);
+
+    // Update isInitialSetup to false (see above)
+    useEffect(() => {
+        if (showRoundSetter) toggleInitialSetup();
+    }, [showRoundSetter]);
 
     return (
         <React.Fragment>
+            {showRoundSetter && (
+                <RoundsNumberSetterModal
+                    isOpen={true}
+                    size={`${width < 1024 ? "1/3" : "1/5"}`}
+                    toggleModal={setShowRoundSetter}
+                    gameResetHandler={shouldStartGame}
+                    showModalClose={false}
+                    unclosable={true}
+                />
+            )}
             <span className="start-blinking-title absolute text-green-300 uppercase text-[2.6vw]">
                 {t("game-interface.start-blinking-title")}
             </span>
