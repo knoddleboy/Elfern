5b46dde3d347220c732275b79fe5706a4fc2dda3
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-08 02:27:29 +0200

    Change names

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,4 +1,7 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
+
+import { useSelector } from "react-redux";
+import { State } from "@state/index";
 
 import useTranslation from "@utils/hooks/useTranslation";
 
@@ -6,18 +9,20 @@ import CustomButton from "@components/CustomButton";
 import CardLinker from "@utils/CardLinker";
 
 import { ReactSetState } from "@src/types";
-import { CYRILLIC_THEME_OFF } from "@constants/applicationTheme";
 
 import "./MainMenu.scss";
 
 const CARD_SIZE = 20;
 const cardShadow = "0px 3px 28px 0px rgba(0, 0, 0, 0.3)";
 
-const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
-    const mainMenuRef = useRef<HTMLDivElement>(null);
-    const [menuAnimationState, setMenuAnimationState] = useState(false);
+const MainMenu: React.FC<{ toggle: ReactSetState }> = ({ toggle }) => {
+    const lang = useSelector((state: State) => state.LANGUAGE);
+    const cyrillic = lang === "ua";
 
     const { t } = useTranslation();
+
+    const mainMenuRef = useRef<HTMLDivElement>(null);
+    const [menuAnimationState, setMenuAnimationState] = useState(false);
 
     return (
         <React.Fragment>
@@ -26,13 +31,13 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     menuAnimationState ? "menu-out-animation" : ""
                 }`}
                 ref={mainMenuRef}
-                onAnimationEnd={() => render((prevState) => !prevState)}
+                onAnimationEnd={() => toggle((prevState) => !prevState)}
             >
                 <div className="left-cards">
                     <CardLinker
                         rank={"jack"}
                         suit={"spades"}
-                        cyrillic={CYRILLIC_THEME_OFF}
+                        cyrillic={cyrillic}
                         cardSize={CARD_SIZE}
                         relativeElement={{ ref: mainMenuRef }}
                         className="absolute bottom-[28.5%] -left-[4.5%] transition-all duration-200 lg:bottom-[38%] rotate-[60deg]"
@@ -41,7 +46,7 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     <CardLinker
                         rank={"queen"}
                         suit={"spades"}
-                        cyrillic={CYRILLIC_THEME_OFF}
+                        cyrillic={cyrillic}
                         cardSize={CARD_SIZE}
                         relativeElement={{ ref: mainMenuRef }}
                         className="absolute bottom-[12%] left-[2.5%] transition-all duration-200 lg:bottom-[16%] rotate-[70deg]"
@@ -50,10 +55,10 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     <CardLinker
                         rank={"king"}
                         suit={"spades"}
-                        cyrillic={CYRILLIC_THEME_OFF}
+                        cyrillic={cyrillic}
                         cardSize={CARD_SIZE}
                         relativeElement={{ ref: mainMenuRef }}
-                        className="absolute -bottom-[5.2%] left-[2%] transition-all duration-200 lg:-bottom-[7%] rotate-90"
+                        className="absolute -bottom-[6%] left-[2%] transition-all duration-200 lg:-bottom-[7%] rotate-90"
                         shadow={`${cardShadow}`}
                     />
                 </div>
@@ -62,7 +67,7 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     <CardLinker
                         rank={"jack"}
                         suit={"hearts"}
-                        cyrillic={CYRILLIC_THEME_OFF}
+                        cyrillic={cyrillic}
                         cardSize={CARD_SIZE}
                         relativeElement={{ ref: mainMenuRef }}
                         className="absolute top-[28.5%] -right-[4.5%] transition-all duration-200 lg:top-[38%] -rotate-[120deg]"
@@ -71,7 +76,7 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     <CardLinker
                         rank={"queen"}
                         suit={"hearts"}
-                        cyrillic={CYRILLIC_THEME_OFF}
+                        cyrillic={cyrillic}
                         cardSize={CARD_SIZE}
                         relativeElement={{ ref: mainMenuRef }}
                         className="absolute top-[12%] right-[2.5%] transition-all duration-200 lg:top-[16%] -rotate-[110deg]"
@@ -80,7 +85,7 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     <CardLinker
                         rank={"king"}
                         suit={"hearts"}
-                        cyrillic={CYRILLIC_THEME_OFF}
+                        cyrillic={cyrillic}
                         cardSize={CARD_SIZE}
                         relativeElement={{ ref: mainMenuRef }}
                         className="absolute -top-[5.2%] right-[2%] transition-all duration-200 lg:-top-[7%] -rotate-90"
@@ -88,11 +93,11 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                     />
                 </div>
 
+                <span className="absolute top-0 font-english-towne text-[15vw] text-[#2b883e]">Elfern</span>
+
                 <div
                     className={`w-full flex justify-center mb-[56px] ${
-                        menuAnimationState
-                            ? "pointer-events-none cursor-pointer"
-                            : "pointer-events-auto cursor-auto"
+                        menuAnimationState ? "pointer-events-none cursor-pointer" : "pointer-events-auto cursor-auto"
                     }`}
                 >
                     <CustomButton
@@ -102,7 +107,7 @@ const MainMenu: React.FC<{ render: ReactSetState }> = ({ render }) => {
                         tracking-wide sm:text-sm md:text-base lg:text-lg xl:text-2xl
                         py-[1.2%] px-[calc(4%)]
                         rounded md:rounded-md lg:rounded-lg"
-                        onClick={() => setMenuAnimationState((prevState) => !prevState)}
+                        onClick={() => setMenuAnimationState((prev) => !prev)}
                     >
                         {t("main-menu.start-button")}
                     </CustomButton>
