85f482a03f5cf5deb4bfae6300ff0a1a406aec8a
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-08 02:11:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
 
-import { ipcRenderer } from "electron";
-
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators, State } from "@state/index";
@@ -13,13 +11,14 @@ import MainMenu from "@src/components/MainMenu";
 import GameInterface from "@components/GameInterface";
 import SavedProgressNotification from "@components/SavedProgressNotification";
 
-import "./App.scss";
+import { globalSettings, globalSettingsDefaults, activeSession, activeSessionDefaults } from "./configs";
+import { DIALOG_FADEOUT_ANIMATION_DURATION } from "./constants";
 
-const { globalSettings, storeDefaults } = require("./configs");
+import "./App.scss";
 
 const App: React.FC = () => {
     const dispatch = useDispatch();
-    const { setRoundStats, toggleInitialSetup, setTimerState, setCurrentScore } = bindActionCreators(
+    const { toggleAudio, setTranslation, toggleInitialSetup, setStats, setProgress } = bindActionCreators(
         actionCreators,
         dispatch
     );
@@ -29,72 +28,68 @@ const App: React.FC = () => {
     // Defines the state of the main (start) menu
     const [mainMenuIsRendered, setMainMenuRenderState] = useState(true);
 
-    // Defined wether to show warning window (see below)
+    // Defined wether to show notifying window with (see below)
     const [savedProcessModalState, setSavedProcessModalState] = useState(false);
 
-    // ...
-    const savedProcessGuard = useRef(false);
-
-    const [renderApp, setRenderApp] = useState(false);
+    const [renderGame, setRenderGame] = useState(false);
 
     // If INITIAL_SETUP does not match with the stored one (which means that a user
-    // once started a game and did not finished it), show warning about saved progress
+    // once started a game and did not finished it), show warning about saved progress.
+    // Otherwise render gameInterface right away
     useEffect(() => {
-        // Send configured store to the main process
-        // ipcRenderer.send("dispatch-main-store", store);
-
-        const savedProgress = store;
-        if (storeDefaults.INITIAL_SETUP !== savedProgress.INITIAL_SETUP) setSavedProcessModalState(true);
-        else savedProcessGuard.current = true;
+        if (activeSessionDefaults.INITIAL_SETUP !== store.INITIAL_SETUP) setSavedProcessModalState(true);
+        else setRenderGame(true);
     }, []);
 
-    useEffect(() => {
-        if (savedProcessGuard.current) setRenderApp(true);
-    }, [savedProcessGuard.current]);
+    // If there is stored session, we indicate that by setting `true`
+    const restoreSession = useRef(false);
+
+    const handleCancel = () => {
+        // If a player chooses 'Cancel', we set saved data to its default value
+        globalSettings.set("ENABLE_AUDIO", globalSettingsDefaults.ENABLE_AUDIO);
+        toggleAudio(globalSettingsDefaults.ENABLE_AUDIO);
+
+        globalSettings.set("LANGUAGE", globalSettingsDefaults.LANGUAGE);
+        setTranslation(globalSettingsDefaults.LANGUAGE);
+
+        activeSession.set("INITIAL_SETUP", activeSessionDefaults.INITIAL_SETUP);
+        toggleInitialSetup(activeSessionDefaults.INITIAL_SETUP);
+
+        activeSession.set("PROGRESS", activeSessionDefaults.PROGRESS);
+        setProgress(activeSessionDefaults.PROGRESS);
+
+        activeSession.set("STATS", activeSessionDefaults.STATS);
+        setStats(activeSessionDefaults.STATS);
+
+        // Close modal after out animation
+        setTimeout(() => setSavedProcessModalState(false), DIALOG_FADEOUT_ANIMATION_DURATION);
+        restoreSession.current = false;
+        setRenderGame(true); // render gameInterface
+    };
+
+    const handleRestore = () => {
+        // Close modal after out animation
+        setTimeout(() => setSavedProcessModalState(false), DIALOG_FADEOUT_ANIMATION_DURATION);
+        restoreSession.current = true;
+        setRenderGame(true); // render gameInterface, restoring stored session
+    };
 
     return (
         <React.Fragment>
             <Titlebar />
-            {savedProcessModalState && (
-                <Modal isOpen={true} size={"1/2"} toggleModal={() => {}} unclosable={true} disableScrollBar sinkTimer>
-                    <SavedProgressNotification
-                        handleCancel={() => {
-                            // If a player chooses 'Cancel', we set saved data to its default value
-                            globalSettings.set("ROUND_STATS", storeDefaults.ROUND_STATS);
-                            setRoundStats(storeDefaults.ROUND_STATS);
-
-                            globalSettings.set("INITIAL_SETUP", storeDefaults.INITIAL_SETUP);
-                            toggleInitialSetup();
-
-                            globalSettings.set("TIMER_STATE", storeDefaults.TIMER_STATE);
-                            setTimerState(storeDefaults.TIMER_STATE);
-
-                            globalSettings.set("CURRENT_SCORE", storeDefaults.CURRENT_SCORE);
-                            setCurrentScore(storeDefaults.CURRENT_SCORE);
-
-                            setTimeout(() => setSavedProcessModalState(false), 300);
-                            savedProcessGuard.current = true;
-                        }}
-                        handleRestore={() => {
-                            setTimeout(() => setSavedProcessModalState(false), 300);
-                            savedProcessGuard.current = true;
-                        }}
-                    />
+            {!mainMenuIsRendered && savedProcessModalState && (
+                <Modal isOpen={true} size={"1/2"} toggle={() => {}} unclosable={true} disableScrollBar>
+                    <SavedProgressNotification handleCancel={handleCancel} handleRestore={handleRestore} />
                 </Modal>
             )}
             <div className="App w-full h-[calc(100vh-1.5rem)] bg-green-500 overflow-hidden">
-                {/* {mainMenuIsRendered ? <MainMenu render={setMainMenuRenderState} /> : renderApp && <GameInterface />}
-                <Sidebar mainMenuState={mainMenuIsRendered} /> */}
-                {renderApp && (
-                    <>
-                        <GameInterface />
-                        <Sidebar mainMenuState={false} />
-                    </>
+                {mainMenuIsRendered ? (
+                    <MainMenu toggle={setMainMenuRenderState} />
+                ) : (
+                    renderGame && <GameInterface restoreSession={restoreSession.current} />
                 )}
+                <Sidebar mainMenuState={mainMenuIsRendered} />
             </div>
-            <button className="bg-dark-800 px-4 py-1 text-white-text" onClick={() => console.log(store)} tabIndex={-1}>
-                Get redux store
-            </button>
         </React.Fragment>
     );
 };
