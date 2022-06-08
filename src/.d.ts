0fe2545ccc203db0f7b07af71e6abff16075e252
Dmitry Knysh <dmitryknysh17@gmail.com>
2022-06-08 02:11:31 +0200

    Add .svg and .md type declaration

--- src/.d.ts
+++ src/.d.ts
@@ -3,7 +3,19 @@ declare module "*.ico" {
     export = value;
 }
 
+declare module "*.svg" {
+    import React from "react";
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const value: string;
+    export default value;
+}
+
 declare module "*.ogg" {
     const value: any;
     export = value;
+}
+
+declare module "*.md" {
+    const value: string;
+    export default value;
 }
